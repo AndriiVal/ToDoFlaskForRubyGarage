04800cbf282f09232d2a9f73afc509a37ee4095f
Andrii Valchuk <andriy.valchuk@gmail.com>
2020-10-20 19:40:00 +0300

    Update for Heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import os
+import logging
+
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 
 from config import Config
@@ -16,6 +20,21 @@ def create_app():
 	app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
 	register_make_shell_context(app)
+
+	if app.config['LOG_TO_STDOUT']:
+		stream_handler = logging.StreamHandler()
+		stream_handler.setLevel(logging.INFO)
+		app.logger.addHandler(stream_handler)
+	else:
+		if not os.path.exists('logs'):
+			os.mkdir('logs')
+		file_handler = RotatingFileHandler('logs/todo.log', maxBytes=10240, backupCount=10)
+		file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s ''[in %(pathname)s:%(lineno)d]'))
+		file_handler.setLevel(logging.INFO)
+		app.logger.addHandler(file_handler)
+
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('ToDo List')
 
 	return app
 
